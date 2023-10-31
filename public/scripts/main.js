8e66409caa64655e3963eb8329d16a4ddfe02d18
rhit-freelahr <freelahr@rose-hulman.edu>
2023-10-31 14:01:04 -0400

    added html pages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,14 @@
 var rhit = rhit || {};
 
+rhit.FB_COLLECTION_BILL = "bill";
+rhit.FB_COLLECTION_INDIVIDUAL = "individual";
+rhit.FB_COLLECTION_GROUP = "group";
+rhit.FB_KEY_NAME = "name";
+rhit.FB_KEY_DESCRIPTION = "description";
+rhit.FB_KEY_AMOUNT = "amount";
+rhit.FB_KEY_FROM = "from";
+
+
 rhit.main = function () {
 
 }
