05b5aa006b1addf628ea7c349c9d1627507e05d4
KP <116210694+rhit-pengkx@users.noreply.github.com>
2023-11-03 09:47:45 -0400

    basic js for login

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,4 +30,105 @@ function drawChart() {
   chart.draw(data, options);
 }
 
+//rhit.fbFinanceManager ------>
+ 
+//rhit.fbExpenseManager ------>
+ 
+//rhit.fbAccountManager ------>
+ 
+rhit.LoginPageController = class {
+  constructor() {
+    document.querySelector("#rosefireButton").onclick = (event) => {
+      rhit.fbAuthManager.signIn();
+    };
+  }
+}
+ 
+rhit.fbAuthManager = class {
+  constructor() {
+    this._user = null;
+  }
+ 
+  beginListening(changeListener) {
+    firebase.auth().onAuthStateChanged((user) => {
+      this._user = user;
+      changeListener();
+    });
+  }
+ 
+  signIn() {
+    console.log("Sign in using Rosefire");
+    Rosefire.signIn("0d7f1ef9-e63b-4cba-ac11-427c8f12b4bf", (err, rfUser) => {
+      if (err) {
+        console.log("Rosefire error!", err);
+        return;
+      }
+      console.log("Rosefire success!", rfUser);
+      firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === 'auth/invalid-custom-token') {
+          alert('The token you provided is not valid.');
+        } else {
+          console.error("Custom auth error", errorCode, errorMessage);
+        }
+      });
+    });
+  }
+ 
+  signOut() {
+    firebase.auth().signOut().then(() => {
+      console.log("You are now signed out");
+    }).catch((error) => {
+      console.log("Sign out error");
+    });
+  }
+  get isSignedIn() {
+    return !!this._user;
+  }
+  get uid() {
+    return this._user.uid;
+  }
+}
+ 
+rhit.checkForRedirects = function () {
+  if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+    window.location.href = "/finance.html"
+  }
+ 
+  if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+    window.location.href = "/"
+  }
+}
+ 
+rhit.initializePage = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (document.querySelector("#financePage")) {
+    const uid = urlParams.get("uid");
+    rhit.fbFinanceManager = new rhit.fbFinanceManager(uid);
+    new rhit.ListPageController();
+  }
+ 
+  if (document.querySelector("#expensePage")) {
+    const captionId = urlParams.get("id");
+    if (!captionId) {
+      window.location.href = "/";
+    }
+    rhit.fbExpenseManager = new rhit.fbExpenseManager(captionId);
+    new rhit.PhotoPageController();
+  }
+ 
+  if (document.querySelector("#accountPage")) {
+    const captionId = urlParams.get("id");
+    if (!captionId) {
+      window.location.href = "/";
+    }
+    rhit.fbAccountManager = new rhit.fbAccountManager(captionId);
+    new rhit.PhotoPageController();
+  }
+  if (document.querySelector("#loginPage")) {
+    new rhit.LoginPageController();
+  }
+}
+
 rhit.main();
